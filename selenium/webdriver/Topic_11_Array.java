1ee2fda88b15b4f447331618fd5e46ac634ffea3
May Mai <maimay0691@gmail.com>
2025-06-28 21:53:14 +0700

    Commit bt

---
+++ selenium/webdriver/Topic_11_Array.java
@@ -0,0 +1,21 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Scanner;
+
+public class Topic_11_Array {
+    WebDriver driver;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main (String[]args) {
+        Topic_09_WhileDoWhile obj = new Topic_09_WhileDoWhile();
+        obj.BT_01();
+    }
+    public static void BT_01(){
+        int num[] = {2,7,6,8,9};
+        for (int i = 0; i<num.length ; i++){
+            
+        }
+        System.out.println();
+    }
+}
